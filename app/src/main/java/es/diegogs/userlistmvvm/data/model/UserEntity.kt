512c3cb4baa880ff699d741d63749fadab18e60f
Diego Gay Saez <diego.gay@gmail.com>
2018-06-10 18:00:09 +0200

    Add model Add user layout Add Rx and Glide dependencies

---
+++ app/src/main/java/es/diegogs/userlistmvvm/data/model/UserEntity.kt
@@ -0,0 +1,6 @@
+package es.diegogs.userlistmvvm.data.model
+
+data class UserEntity(
+        val name: String,
+        val avatarUrl: String
+)
---
+++ app/src/main/java/es/diegogs/userlistmvvm/data/repository/datasource/FakeDataSource.kt
@@ -0,0 +1,4 @@
+package es.diegogs.userlistmvvm.data.repository.datasource
+
+class FakeDataSource {
+}
