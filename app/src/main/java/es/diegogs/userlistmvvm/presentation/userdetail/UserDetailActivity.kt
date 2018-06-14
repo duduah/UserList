b75e9abdeae6f16620e99f6437621f2b29e30994
Diego Gay Saez <diego.gay@gmail.com>
2018-06-14 22:03:25 +0200

    User detail layout created. Activity inflates this layout.

---
+++ app/src/main/java/es/diegogs/userlistmvvm/presentation/userdetail/UserDetailActivity.kt
@@ -0,0 +1,15 @@
+package es.diegogs.userlistmvvm.presentation.userdetail
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import es.diegogs.userlistmvvm.R
+
+class UserDetailActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_user_detail)
+
+    }
+}
