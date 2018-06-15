3320b3f0b093a898e2426986793cf6b5c32af319
Diego Gay Saez <diego.gay@gmail.com>
2018-06-15 19:57:44 +0200

    Adding UserApiResponse and User data model, and defining Retrofit service.

--- app/src/main/java/es/diegogs/userlistmvvm/presentation/servicelocator/Inject.kt
+++ app/src/main/java/es/diegogs/userlistmvvm/presentation/servicelocator/Inject.kt
@@ -1,11 +1,23 @@
 package es.diegogs.userlistmvvm.presentation.servicelocator
 
+import es.diegogs.userlistmvvm.data.net.UserService
 import es.diegogs.userlistmvvm.data.repository.UserRepository
 import es.diegogs.userlistmvvm.data.repository.datasource.UserFakeDataSource
 import es.diegogs.userlistmvvm.data.repository.datasource.UserFakeDataSource2
 import es.diegogs.userlistmvvm.util.SettingsManager
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 object Inject {
+
+    val retrofit = Retrofit.Builder()
+            .baseUrl("https://randomuser.me/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    val userService = retrofit.create(UserService::class.java)
 
     lateinit var settingsManager: SettingsManager
 
