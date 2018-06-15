e430f202974c3493dbb0ced91e2206accde567a2
Diego Gay Saez <diego.gay@gmail.com>
2018-06-15 18:18:53 +0200

    Add user detail layout and activity. Using view model for user detail to practice.

--- app/src/main/java/es/diegogs/userlistmvvm/presentation/userdetail/UserDetailActivity.kt
+++ app/src/main/java/es/diegogs/userlistmvvm/presentation/userdetail/UserDetailActivity.kt
@@ -1,15 +1,62 @@
 package es.diegogs.userlistmvvm.presentation.userdetail
 
+import android.app.Activity
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import es.diegogs.userlistmvvm.R
+import es.diegogs.userlistmvvm.data.model.UserEntity
+import kotlinx.android.synthetic.main.activity_user_detail.*
 
 class UserDetailActivity: AppCompatActivity() {
+
+    companion object {
+        const val PARAM_USER_ID = "PARAM_USER_ID"
+    }
+
+    lateinit var userDetailViewModel: UserDetailViewModel
+    var userId: Long = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_user_detail)
 
+        userId = intent.getLongExtra(PARAM_USER_ID, -1)
+        init()
+    }
+
+    private fun init() {
+        userDetailViewModel = ViewModelProviders.of(this).get(UserDetailViewModel::class.java)
+        bindEvents()
+        loadUserData()
+    }
+
+    private fun bindEvents() {
+        userDetailViewModel.userState.observe(this, Observer {
+            it?.let {
+                onUserLoaded(it)
+            }
+        })
+    }
+
+    private fun onUserLoaded(userEntity: UserEntity) {
+        userDetailName.text = userEntity.name
+
+        Glide.with(this)
+                .load(userEntity.avatarUrl)
+                .into(userDetailAvatar)
+    }
+
+    private fun loadUserData() {
+
+        if (userId == -1L) {
+            setResult(Activity.RESULT_CANCELED)
+            finish()
+        }
+
+        userDetailViewModel.loadUserById(userId)
     }
 }
