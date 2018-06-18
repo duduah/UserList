d783592aa2ed800fa1130db115d18cfa6a2b2298
Diego Gay Saez <diego.gay@gmail.com>
2018-06-18 18:19:21 +0200

    Adding Parcelize extension and rewriting UserEntity

--- app/src/main/java/es/diegogs/userlistmvvm/data/model/UserEntity.kt
+++ app/src/main/java/es/diegogs/userlistmvvm/data/model/UserEntity.kt
@@ -2,32 +2,10 @@ package es.diegogs.userlistmvvm.data.model
 
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class UserEntity(
         val name: String,
         val avatarUrl: String
-): Parcelable {
-    constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readString()) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeString(avatarUrl)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<UserEntity> {
-        override fun createFromParcel(parcel: Parcel): UserEntity {
-            return UserEntity(parcel)
-        }
-
-        override fun newArray(size: Int): Array<UserEntity?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+): Parcelable
